c3c099ff029c7ea2f93a6f5cb3165fb0ab292ea0
renl <renl@umich.edu>
2015-01-17 04:18:45 -0500

    4:18AM

--- ideaMeet/urls.py
+++ ideaMeet/urls.py
@@ -7,6 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'index.views.index'),
-    url(r'^core/', 'core.views.index'),
+    url(r'^$', 'core.views.index'),
 )
