c2c9b297406f7790cabc400deb7c17b3ff97f719
renl <renl@umich.edu>
2015-01-17 16:10:11 -0500

    save finished

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from utility import *
 
 # Create your views here.
 def index(request):
+  saveSubmitData(request)
   return render(request, 'core.html')
